9b28adaabeb4999907a96afccaf92df0f359888a
jwy <lygjwy@qq.com>
2022-01-09 09:51:14 +0800

    fix bugs

--- analyse_result.py
+++ analyse_result.py
@@ -16,6 +16,7 @@ def draw_line(data, shape, label):
     plt.plot(max_idx, data[max_idx], 'bs')
     max_show_text = '('+str(max_idx)+','+str(data[max_idx])+')'
     plt.annotate(max_show_text, xy=(max_idx, data[max_idx]))
+    plt.plot(min_idx, data[min_idx], 'ks')
     min_show_text = '('+str(min_idx)+','+str(data[min_idx])+')'
     plt.annotate(min_show_text, xy=(min_idx, data[min_idx]))
 
@@ -71,8 +72,8 @@ for paradigm in paradigms:
                 _, rec_err, _, cla_acc = re.findall(pattern, line) 
                 cla_accs.append(float(cla_acc))
                 rec_errs.append(float(rec_err))
-        train_cla_accs, train_rec_errs = cla_accs[::2], cla_accs[1::2]
-        test_cla_accs, test_rec_errs = rec_errs[::2], rec_errs[1::2]
+        train_cla_accs, test_cla_accs = cla_accs[::2], cla_accs[1::2]
+        train_rec_errs, test_rec_errs = rec_errs[::2], rec_errs[1::2]
         # plot cla_acc-epoch.png & rec_err-epoch.png
         double_visualize(train_cla_accs, test_cla_accs, 'train-acc', 'test-acc', 'epoch', 'accuracy', str(exp_path / 'cla_accs.png'))
         double_visualize(train_rec_errs, test_rec_errs, 'train-rec', 'test-rec', 'epoch', 'rec_err', str(exp_path / 'rec_errs.png'))
@@ -84,13 +85,13 @@ for paradigm in paradigms:
             epoch_path = exp_path / str(i)
             # read all data set
             id_logits = np.load(str(epoch_path / ('-'.join(['logits', id]) + '.npy')))
-            print(id_logits.shape)
+            # print(id_logits.shape)
             total_fpr_at_tpr, total_auroc, total_aupr_in, total_aupr_out = [], [], [], []
             for ood in oods:
                 ood_logits = np.load(str(epoch_path / ('-'.join(['logits', ood]) + '.npy')))
                 id_logits, ood_logits = torch.from_numpy(id_logits), torch.from_numpy(ood_logits)
-                print(id_logits.shape)
-                exit()
+                # print(id_logits.shape)
+                # exit()
                 # calculate mean auroc & aupr_in & aupr_out & fpr_tpr
                 id_scores = torch.max(torch.softmax(id_logits, dim=1), dim=1)[0].numpy()
                 id_labels = np.zeros(id_scores.shape[0])
